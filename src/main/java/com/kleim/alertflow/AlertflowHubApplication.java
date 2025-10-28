e044c5974ce2c120ce4d58359ea9b075d09aa5fb
Vladimir <kitkatorgnew@gmail.com>
2025-10-28 22:50:55 +0300

    initial commit

---
+++ src/main/java/com/kleim/alertflow/AlertflowHubApplication.java
@@ -0,0 +1,13 @@
+package com.kleim.alertflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlertflowHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlertflowHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kleim/alertflow/AlertflowHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kleim.alertflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlertflowHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
